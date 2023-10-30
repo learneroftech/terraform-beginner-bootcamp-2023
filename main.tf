370a131057d679e2e17840ca5d4559af9782f991
learneroftech <145515536+learneroftech@users.noreply.github.com>
2023-10-30 07:14:50 -0700

    # migrate local state to terraform cloud using terraform cloud block (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "testtf-organization"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  cloud {
+    organization = "testtf-organization"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
