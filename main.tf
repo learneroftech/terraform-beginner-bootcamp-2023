615db8caa410f9ff813ab2b2c88e1863651e76a4
Viet Nguyen <viet.techlearner@gmail.com>
2023-12-28 05:25:46 +0000

    comment out more

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-}
+  #index_html_filepath = var.index_html_filepath
+  #error_html_filepath = var.error_html_filepath
+}#
 
 # My Personal Edit: Here's my Terraform Token that expires 1/26/2023: JmdKut9dr3S9uQ.atlasv1.du1zenNy8TxtE6g0Bem7GETlwVHBf43vqA3h6l2M33j0LylDLnI13HUTAi1i8ygPpzQ
