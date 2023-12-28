faaf0a7ef917098f9ea2d653b112743369ad56cf
Viet Nguyen <viet.techlearner@gmail.com>
2023-12-28 05:52:43 +0000

    yes!

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -16,23 +16,23 @@ variable "bucket_name" {
     error_message = "The bucket name must be a valid S3 bucket name: lowercase letters, numbers, and hyphens, between 3 and 63 characters."
   }
 }
-
-variable "index_html_filepath" {
-  description = "The file path for index.html"
-  type        = string
-
-  validation {
-    condition     = fileexists(var.index_html_filepath)
-    error_message = "The provided path for index.html does not exist"
-  }
-}
-
-variable "error_html_filepath" {
-  description = "The file path for error.html"
-  type        = string
-
-  validation {
-    condition     = fileexists(var.error_html_filepath)
-    error_message = "The provided path for error.html does not exist"
-  }
-}
+#
+#variable "index_html_filepath" {
+#  description = "The file path for index.html"
+#  type        = string
+#
+#  validation {
+#    condition     = fileexists(var.index_html_filepath)
+#    error_message = "The provided path for index.html does not exist"
+#  }
+#}
+#
+#variable "error_html_filepath" {
+#  description = "The file path for error.html"
+#  type        = string
+#
+#  validation {
+#    condition     = fileexists(var.error_html_filepath)
+#    error_message = "The provided path for error.html does not exist"
+#  }
+#}
