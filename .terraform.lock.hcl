e127dd0bd7d4e9a0af2cb8bfa71b6ce7c703b43a
Viet Nguyen <viet.techlearner@gmail.com>
2024-01-02 02:56:17 +0000

    #47 finish implementation for terraform provider

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:VCji3+R4Af7kyOG3rwVtoK+6CStHrw06zBrxEH9p8+w=",
+    "h1:rg/wtp7Q0n8yLm/viUh2E8kbnrOSw/pJpaHmkFRasdI=",
   ]
 }
--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
 terraform {
   required_providers {
     terratowns = {
-      source  = "local/providers/terratowns"
+      source = "local.providers/local/terratowns"
       version = "1.0.0"
     }
   }
-
-  
   #backend "remote" {
   #  hostname = "app.terraform.io"
-  #  organization = "testtf-organization"
+  #  organization = "ExamPro"
 
   #  workspaces {
   #    name = "terra-house-1"
   #  }
   #}
   #cloud {
-  #  organization = "testtf-organization"
+  #  organization = "ExamPro"
   #  workspaces {
   #    name = "terra-house-1"
   #  }
@@ -25,57 +23,12 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http:localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
-
-#provider "terratowns" {
-#  endpoint = var.terratowns_endpoint
-#  user_uuid = var.teacherseat_user_uuid
-#  token = var.terratowns_access_token
-#}
-
-#module "home_arcanum_hosting" {
-#  source = "./modules/terrahome_aws"
-#  user_uuid = var.teacherseat_user_uuid
-#  public_path = var.arcanum.public_path
-#  content_version = var.arcanum.content_version
-#}
-
-#resource "terratowns_home" "home" {
-#  name = "How to play Arcanum in 2023!"
-#  description = <<DESCRIPTION
-#Arcanum is a game from 2001 that shipped with alot of bugs.
-#Modders have removed all the originals making this game really fun
-#to play (despite that old look graphics). This is my guide that will
-#show you how to play arcanum without spoiling the plot.
-#DESCRIPTION
-#  domain_name = module.home_arcanum_hosting.domain_name
-#  town = "missingo"
-#  content_version = var.arcanum.content_version
-#}
-
-#module "home_payday_hosting" {
-#  source = "./modules/terrahome_aws"
-#  user_uuid = var.teacherseat_user_uuid
-#  public_path = var.payday.public_path
-#  content_version = var.payday.content_version
-#}
-
-#resource "terratowns_home" "home_payday" {
-#  name = "Making your Payday Bar"
-#  description = <<DESCRIPTION
-#Since I really like Payday candy bars but they cost so much to import
-#into Canada, I decided I would see how I could my own Paydays bars,
-#and if they are most cost effective.
-#DESCRIPTION
-#  domain_name = module.home_payday_hosting.domain_name
-#  town = "missingo"
-#  content_version = var.payday.content_version
-#}
-##module "terrahouse_aws" {
+#module "terrahouse_aws" {
 #  source = "./modules/terrahouse_aws"
 #  user_uuid = var.user_uuid
 #  bucket_name = var.bucket_name
@@ -83,3 +36,17 @@ provider "terratowns" {
 #  error_html_filepath = var.error_html_filepath
 #  content_version = var.content_version
 #}
+
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
