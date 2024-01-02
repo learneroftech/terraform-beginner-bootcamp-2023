9ec7120524a41754b44fec321003eaa38d3500c0
learneroftech <145515536+learneroftech@users.noreply.github.com>
2024-01-01 19:07:12 -0800

    47 terratowns provider (#49)
    * #47 wip setting up the skeleton for creating a custom terraform provider
    
    * #47 made changes; trying to start terraform init
    
    * #47 another attempt
    
    * #47 deleted       # source ./bin/build_provider
    
    * #47 now the comment works
    
    * #47 returned to original yml code
    
    * #47 implement terraform provider block in terraform
    
    * #47 new edits
    
    * #47 more edits
    
    * #47 more edits
    
    * #47 edited main.go
    
    * #47 save another edit       source  = "local/providers/terratowns"
    
    * #47 more changes
    
    * #47 copied code from omenking
    
    * #47 finish implementation for terraform provider

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ terraform {
   
   #backend "remote" {
   #  hostname = "app.terraform.io"
-  #  organization = "testtf-organization"
+  #  organization = "ExamPro"
 
   #  workspaces {
   #    name = "terra-house-1"
   #  }
   #}
   #cloud {
-  #  organization = "testtf-organization"
+  #  organization = "ExamPro"
   #  workspaces {
   #    name = "terra-house-1"
   #  }
@@ -25,57 +25,12 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http:localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
-
-#provider "terratowns" {
-#  endpoint = var.terratowns_endpoint
-#  user_uuid = var.teacherseat_user_uuid
-#  token = var.terratowns_access_token
-#}
-
-#module "home_arcanum_hosting" {
-#  source = "./modules/terrahome_aws"
-#  user_uuid = var.teacherseat_user_uuid
-#  public_path = var.arcanum.public_path
-#  content_version = var.arcanum.content_version
-#}
-
-#resource "terratowns_home" "home" {
-#  name = "How to play Arcanum in 2023!"
-#  description = <<DESCRIPTION
-#Arcanum is a game from 2001 that shipped with alot of bugs.
-#Modders have removed all the originals making this game really fun
-#to play (despite that old look graphics). This is my guide that will
-#show you how to play arcanum without spoiling the plot.
-#DESCRIPTION
-#  domain_name = module.home_arcanum_hosting.domain_name
-#  town = "missingo"
-#  content_version = var.arcanum.content_version
-#}
-
-#module "home_payday_hosting" {
-#  source = "./modules/terrahome_aws"
-#  user_uuid = var.teacherseat_user_uuid
-#  public_path = var.payday.public_path
-#  content_version = var.payday.content_version
-#}
-
-#resource "terratowns_home" "home_payday" {
-#  name = "Making your Payday Bar"
-#  description = <<DESCRIPTION
-#Since I really like Payday candy bars but they cost so much to import
-#into Canada, I decided I would see how I could my own Paydays bars,
-#and if they are most cost effective.
-#DESCRIPTION
-#  domain_name = module.home_payday_hosting.domain_name
-#  town = "missingo"
-#  content_version = var.payday.content_version
-#}
-##module "terrahouse_aws" {
+#module "terrahouse_aws" {
 #  source = "./modules/terrahouse_aws"
 #  user_uuid = var.user_uuid
 #  bucket_name = var.bucket_name
